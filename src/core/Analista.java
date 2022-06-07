149ba8bdbd2b0a1bd82bbe77ca1e04b3b7c1916a
Marcos4481 <marcos.lacerda@estudiantes.utec.edu.uy>
2022-06-07 16:32:31 -0300

    subiendo archivo Analista

--- src/core/Analista.java
+++ src/core/Analista.java
@@ -1,5 +1,36 @@
 package core;
 
-public class Analista {
+public class Analista extends Usuario {
 
+	private Integer id;
+	private String tipoDeAsistencia;
+	
+	
+	public Analista() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+	public Analista(Integer id, String tipoDeAsistencia) {
+		super();
+		this.id = id;
+		this.tipoDeAsistencia = tipoDeAsistencia;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getTipoDeAsistencia() {
+		return tipoDeAsistencia;
+	}
+	public void setTipoDeAsistencia(String tipoDeAsistencia) {
+		this.tipoDeAsistencia = tipoDeAsistencia;
+	}
+
+	
+	
+	
 }
