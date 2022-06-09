57fc6c3a9ad162f3dc091e6434c83b3a97d6f4d3
Marcos4481 <marcos.lacerda@estudiantes.utec.edu.uy>
2022-06-09 20:41:25 -0300

    Reclamos

--- src/core/Constancia.java
+++ src/core/Constancia.java
@@ -1,5 +1,27 @@
 package core;
 
 public class Constancia {
+	
+	
+ private Integer Id;
+
+public Constancia(Integer id) {
+	super();
+	Id = id;
+}
+
+public Integer getId() {
+	return Id;
+}
+
+public void setId(Integer id) {
+	Id = id;
+}
+ 
+ 
+ 
+ 
+	
+	
 
 }
